4bcef4a33d78758fd5458aa62ffc2881b0326344
Murthy G <mur33kittu@gmail.com>
2021-10-27 12:22:59 -0400

    PATCH: json

--- hackathon-ui/src/actions/types.js
+++
@@ -1,9 +0,0 @@
-export const GET_PRODUCTS = 'GET_PRODUCTS';
-export const ADD_TO_CART = 'ADD_TO_CART';
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
-export const CLEAR_CART = 'CLEAR_CART';
-export const GET_PLACED_ORDERS = 'GET_PLACED_ORDERS';
-export const GET_TRACKING_INFO = 'GET_TRACKING_INFO';
-export const CHANGE_AUTH = 'CHANGE_AUTH';
-export const GET_USER_INFO = 'GET_USER_INFO';
-export const GET_PAYMENT_INFO = 'GET_PAYMENT_INFO';
